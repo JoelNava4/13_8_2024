f7c4e6977fcc91e2078777c3021581266067bfc6
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2024-08-13 10:10:11 -0400

    Añadimos multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,25 @@
-import sumar from "./sumador";
+import {sumar,multiplicar} from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const select = document.querySelector("#operaciones-options");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const selectedOption = select.value;
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  
+  let result;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  if (selectedOption === "sumar") {
+    result = sumar(firstNumber, secondNumber);
+  } else if (selectedOption === "multiplicar") {
+    result = multiplicar(firstNumber, secondNumber);
+  }
+
+  div.innerHTML = "<p>" + result + "</p>";
 });
